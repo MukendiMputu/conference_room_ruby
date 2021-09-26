051e5d52820e322fdc397ee8899d4855959d5b64
MukendiMputu <mukendi_mputu@outlook.com>
2021-09-26 03:39:58 +0200

    corrects loadError with -listen

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,7 +12,6 @@ Rails.application.configure do
   # Whitelist one hostname
   config.hosts << "archmukki-localnet.de"
 
-
   # Show full error reports.
   config.consider_all_requests_local = true
 
@@ -62,5 +61,5 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
