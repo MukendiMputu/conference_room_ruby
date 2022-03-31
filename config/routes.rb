4c513de3859cabc079aad54055f382736a87e29a
MukendiMputu <mukendi_mputu@outlook.com>
2022-03-31 23:03:40 +0200

    adds missing resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+
+  root to: 'welcome#index', as: 'home'
   post 'welcome/search_booking'
+
+  get 'login', to: 'login#index', as: 'login'
+
+  resources :users, only: [:index, :show, :create, :destroy]
+  resources :rooms, only: :show
+  resources :booking
+
 end
