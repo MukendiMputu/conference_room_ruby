b235db7f546052f64170b9e9b477aafdbb1d7cbc
MukendiMputu <mukendi_mputu@outlook.com>
2022-04-01 22:50:08 +0200

    adds database start up data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+rooms = Room.create([
+                      { building: 'RIC', number: 'CS310', capacity: 50, configuration: 'Circle', picture: 'conference_blue.jpg', description: 'example room' },
+                      { building: 'SEC', number: 'CS310', capacity: 25, configuration: 'U-Shape', picture: 'conference_green.jpg', description: 'example room' },
+                      { building: 'Riddell', number: 'CS310', capacity: 100, configuration: 'Conference', picture: 'conference_royal.jpg', description: 'example room' },
+                    ])
