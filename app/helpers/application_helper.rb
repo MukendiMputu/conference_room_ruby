e51db9a5a88b8fd272fd9689d39b741f2175d431
MukendiMputu <mukendi_mputu@outlook.com>
2022-04-25 00:04:07 +0200

    adds application helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  # Defining a method to render the error display partial
+  def error_messages_for(object)
+    render(
+      partial: "application/error_messages",
+      locals: {object: object}
+    )
+  end
+
+  # Reusing the main header
+  def show_default_header
+    render(partial: "application/header")
+  end
+
+  # confirm user session
+  def is_logged_in
+    session[:user_id].present?
+  end
 end
